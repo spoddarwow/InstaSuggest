7dd44a4267e3b5d75aeff7b5af63c60ba8da800b
xspro <xspro@NNORSOWGLT568.woolworths.com.au>
2016-01-19 16:45:37 +1100

    First Hibernate imple

--- src/com/suggesthashtag/db/hibernate/domain/Demo.java
+++ src/com/suggesthashtag/db/hibernate/domain/Demo.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
@@ -32,15 +35,27 @@ public class Demo {
 		property.load(inputStream);
 		System.out.println(property);
 		try {
-			
-			factory = new AnnotationConfiguration()
-					.configure(new File("C:\\Sumit\\Git_Repo_Luna\\230814\\SuggestHashTag\\config\\demo.properties"))
-					//.addPackage("com.suggesthashtag.db.hibernate.domain")
-					//.addAnnotatedClass(Student.class)
+			factory = new AnnotationConfiguration().mergeProperties(property)
+					.configure("hibernate.cfg.xml")
+					// .addPackage("com.suggesthashtag.db.hibernate.domain")
+					// .addAnnotatedClass(Student.class)
 					.buildSessionFactory();
+			Session session = factory.getCurrentSession();
+			Student student = new Student(10001, "Sumit Poddar", 19);
+			try {
+
+				session.beginTransaction();
+				session.save(student);
+				session.getTransaction().commit();
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			}
 		} catch (Throwable ex) {
 			System.err.println("Failed to create sessionFactory object." + ex);
 			throw new ExceptionInInitializerError(ex);
+		} finally {
+			factory.close();
 		}
 		System.out.println(factory);
 	}
--- src/com/suggesthashtag/db/hibernate/domain/Student.java
+++ src/com/suggesthashtag/db/hibernate/domain/Student.java
@@ -23,9 +23,65 @@ public class Student {
 	@Column(name = "id")
 	private long id;
 
+	@Column(name = "studentName")
+	private String name;
+
+	@Column(name = "age")
+	private int age;
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param age
+	 */
+	public Student(long id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
 	public long getId() {
 		return id;
 
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return this.age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
 	}
 
 }
